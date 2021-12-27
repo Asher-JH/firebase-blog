2f58453af2777435c266747e28dd2bac37f8dc4d
Asher Chan <asherchan2002@hotmail.com>
2021-12-27 17:44:15 +0800

    Re-build client app & fix import aliases problem

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import * as React from 'react';
+import {
+    ChakraProvider,
+    Box,
+    Text,
+    Link,
+    VStack,
+    Code,
+    Grid,
+    theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
 
-function App() {
-    return (
-        <ChakraProvider>
-            <App />
-        </ChakraProvider>
-    );
-}
+import Navbar from '@components/nav-bar';
+
+export const App = () => (
+    <ChakraProvider theme={theme}>
+        <Navbar />
+        <Box textAlign="center" fontSize="xl">
+            <Grid minH="100vh" p={3}>
+                <ColorModeSwitcher justifySelf="flex-end" />
+                <VStack spacing={8}>
+                    <Text>
+                        Edit <Code fontSize="xl">src/App.tsx</Code> and save to
+                        reload.
+                    </Text>
+                    <Link
+                        color="teal.500"
+                        href="https://chakra-ui.com"
+                        fontSize="2xl"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn Chakra
+                    </Link>
+                </VStack>
+            </Grid>
+        </Box>
+    </ChakraProvider>
+);
